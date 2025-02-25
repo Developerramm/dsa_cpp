753db6a220f77450922943c0b387733f878ba308
ram kumar <rr697144@gmail.com>
2025-02-25 21:40:30 +0530

    data type in cpp

---
+++ Basic/dataType.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    char ch = 'A';
+    float salary = 34324.34;
+    double sl = 4323243.43243;
+    bool isProgram = true;
+
+    short num1 = 23;
+
+    cout << a << endl;
+    cout << ch << endl;
+    cout << salary << endl;
+    cout << sl << endl;
+    cout << isProgram << endl;
+
+    long long int num = 4324243532423l;
+    cout << num << endl;
+
+    return 0;
+}
