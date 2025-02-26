0db208e20f1bd2dc1dd0209000359b8a702d15f4
ram kumar <rr697144@gmail.com>
2025-02-26 13:14:18 +0530

    type casting

---
+++ Basic/main1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    char ch = 97;
+    cout << ch << endl;
+
+    int num1 = 'A';
+    cout << num1 << endl;
+
+    double d = 5.3;
+    int x = (int) d + 2;
+    cout << x << endl;
+
+    return 0;
+}
