01b40c87379aa86130ced88660617cb92b3b0600
ram kumar <rr697144@gmail.com>
2025-02-27 16:05:15 +0530

    printing counting using for loop

---
+++ Pattern/main1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "print counting " << endl;
+    for (int i = 1; i <= 100; i++)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    return 0;
+}
